2ee97cb17d87460f9a277620fc3cc6104a609c40
authoravp1-png <authoravp1@gmail.com>
2026-04-26 16:15:21 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, jsonify
+import requests
+import uuid
+import os
+
+app = Flask(__name__)
+
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+
+headers = {
+    "apikey": SUPABASE_KEY,
+    "Authorization": f"Bearer {SUPABASE_KEY}",
+    "Content-Type": "application/json"
+}
+
+@app.route("/")
+def home():
+    return "Health Profile API Running"
+
+@app.route("/create_user", methods=["POST"])
+def create_user():
+    data = request.json
+
+    user = {
+        "id": str(uuid.uuid4()),
+        "name": data["name"],
+        "age": data["age"],
+        "gender": data["gender"],
+        "blood_group": data["blood_group"],
+        "abha_id": data["abha_id"]
+    }
+
+    res = requests.post(
+        f"{SUPABASE_URL}/rest/v1/users",
+        json=user,
+        headers=headers
+    )
+
+    return jsonify(res.json())
+
+app.run(host="0.0.0.0", port=5000)
